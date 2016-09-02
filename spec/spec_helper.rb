c8673101b47bc49cc69c27131648c2239b179465
Thibaut Roche <thibaut.roche.perso@gmail.com>
2016-09-02 11:42:38 +0200

    [A] Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,21 @@
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.minimum_coverage 90
+SimpleCov.minimum_coverage_by_file 80
+SimpleCov.add_filter '/spec/timeline/'
+
+SimpleCov.formatters = [
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.at_exit do
+  Dir.mkdir('coverage') unless Dir.exist?('coverage')
+  SimpleCov.result.format!
+end
+
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'timeline/rails'
