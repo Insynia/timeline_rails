51b9cfa192e89e2c6581a3f272192d640b7bd00d
Thibaut Roche <thibaut.roche.perso@gmail.com>
2016-09-02 11:25:56 +0200

    [F] Removed base tests

--- spec/timeline/rails_spec.rb
+++ spec/timeline/rails_spec.rb
@@ -4,8 +4,4 @@ describe Timeline::Rails do
   it 'has a version number' do
     expect(Timeline::Rails::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
